9065c2fe1d2270dcee25f93f93a9aad8dcb1c980
unknown <trex33160@gmail.com>
2021-03-16 21:53:47 +0545

    made node js sevrer

---
+++ index.js
@@ -0,0 +1,12 @@
+const { readFile } = require('fs').promises;
+const express = require('express');
+
+const app = express();
+
+app.get('/', async(request, response) => {
+
+    response.send(await readFile('./home.html', 'utf8'));
+}
+)
+
+app.listen(process.env.PORT || 3000, () => {console.log(`App available on http://localhost:3000`)})
